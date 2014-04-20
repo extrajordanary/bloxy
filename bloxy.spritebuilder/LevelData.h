749ef4ef71df554f67a998adc74ceedc23eee1d7
dott <dott@mail.ccsf.edu>
2014-04-20 01:43:15 -0700

    before scrolling implemented

---
+++ bloxy.spritebuilder/LevelData.h
@@ -0,0 +1,13 @@
+//
+//  LevelData.h
+//  bloxy
+//
+//  Created by David Ott on 4/20/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelData : NSObject
+
+@end
---
+++ bloxy.spritebuilder/LevelData.m
@@ -0,0 +1,13 @@
+//
+//  LevelData.m
+//  bloxy
+//
+//  Created by David Ott on 4/20/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "LevelData.h"
+
+@implementation LevelData
+
+@end
